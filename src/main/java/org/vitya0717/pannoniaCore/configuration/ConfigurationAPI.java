6e0748540158bff5f372e641d895121022bc975d
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2025-12-06 22:12:20 +0100

    ConfigurationAPI

--- src/main/java/org/vitya0717/pannoniaCore/configuration/ConfigurationAPI.java
+++ src/main/java/org/vitya0717/pannoniaCore/configuration/ConfigurationAPI.java
@@ -3,27 +3,27 @@ package org.vitya0717.pannoniaCore.configuration;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.vitya0717.pannoniaCore.main.Main;
-import org.vitya0717.pannoniaCore.moduleManager.PannoniaModule;
-import org.vitya0717.pannoniaCore.utils.ConsoleUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.logging.Logger;
 
+@SuppressWarnings("ALL")
 public class ConfigurationAPI {
 
-    private File configFile;
+    private final File configFile;
     private final Class<?> module;
-    private File configFolder;
+    private final File configFolder;
     private FileConfiguration fileConfiguration;
 
     public ConfigurationAPI(Class<?> module, String moduleName, String configFileName) {
         this.module = module;
-        this.configFolder = new File(Main.getPlugin(Main.class).getDataFolder()+"/modules", moduleName);
+        this.configFolder = new File(Main.getPlugin(Main.class).getDataFolder()+"/modules/", moduleName);
         this.configFile = new File(configFolder, configFileName);
     }
 
@@ -32,24 +32,27 @@ public class ConfigurationAPI {
             configFolder.mkdirs();
         }
 
-        try (InputStream in = module.getResourceAsStream("/config.yml")) {
+        if(!configFile.getName().endsWith(".yml")) try {
+            throw new InvalidConfigurationException();
+        } catch (InvalidConfigurationException e) {
+            Logger.getLogger("PannoniaCore").severe("Sorry, only YAML files are supported at this time.");
+        }
 
-            if (in == null) {
-                ConsoleUtils.SendConsole("&cNull");
-                fileConfiguration = new YamlConfiguration();
-                fileConfiguration.save(configFile);
-                return;
-            }
+        try (InputStream in = module.getResourceAsStream("/"+configFile.getName())) {
+
+            if (in == null) throw new FileNotFoundException();
 
            if(!configFile.exists()) {
-               configFile.mkdirs();
+               Files.copy(in, configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
            }
 
            fileConfiguration = new YamlConfiguration();
-
            fileConfiguration.load(configFile);
 
-        } catch (IOException | InvalidConfigurationException e) {
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            Logger.getLogger("PannoniaCore").severe("Error occurred while trying to load configuration.");
             e.printStackTrace();
         }
     }
@@ -57,6 +60,34 @@ public class ConfigurationAPI {
     public FileConfiguration getConfig() {
         return fileConfiguration;
     }
+
+    public void reloadConfig() {
+        fileConfiguration = null;
+
+        fileConfiguration = new YamlConfiguration();
+
+        try {
+
+            if(!configFile.exists())  {
+                saveDefaultConfig();
+                return;
+            }
+
+            fileConfiguration.load(configFile);
+        } catch (IOException | InvalidConfigurationException e) {
+            Logger.getLogger("PannoniaCore").severe("Error occurred trying to reload configuration.");
+            e.printStackTrace();
+        }
+    }
+
+    public void saveConfig() {
+        try {
+            fileConfiguration.save(configFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public File getFile() {
         return configFile;
