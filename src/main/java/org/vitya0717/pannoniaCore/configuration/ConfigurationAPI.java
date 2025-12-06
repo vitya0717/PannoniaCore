ccf7bcc3744542060d5ad22ca5b856a2c977c479
Tóth Viktor <50296138+vitya0717@users.noreply.github.com>
2025-12-06 03:50:46 +0100

    ConfigurationAPI

--- src/main/java/org/vitya0717/pannoniaCore/configuration/ConfigurationAPI.java
+++ src/main/java/org/vitya0717/pannoniaCore/configuration/ConfigurationAPI.java
@@ -1,8 +1,12 @@
 package org.vitya0717.pannoniaCore.configuration;
 
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.vitya0717.pannoniaCore.main.Main;
+import org.vitya0717.pannoniaCore.moduleManager.PannoniaModule;
+import org.vitya0717.pannoniaCore.utils.ConsoleUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,11 +17,13 @@ import java.nio.file.StandardCopyOption;
 public class ConfigurationAPI {
 
     private File configFile;
+    private final Class<?> module;
     private File configFolder;
     private FileConfiguration fileConfiguration;
 
-    public ConfigurationAPI(JavaPlugin plugin, String moduleName, String configFileName) {
-        this.configFolder = new File(plugin.getDataFolder().getParentFile(), moduleName);
+    public ConfigurationAPI(Class<?> module, String moduleName, String configFileName) {
+        this.module = module;
+        this.configFolder = new File(Main.getPlugin(Main.class).getDataFolder()+"/modules", moduleName);
         this.configFile = new File(configFolder, configFileName);
     }
 
@@ -26,14 +32,24 @@ public class ConfigurationAPI {
             configFolder.mkdirs();
         }
 
-        try (InputStream in = getClass().getResourceAsStream("/config.yml")) {
+        try (InputStream in = module.getResourceAsStream("/config.yml")) {
+
             if (in == null) {
+                ConsoleUtils.SendConsole("&cNull");
                 fileConfiguration = new YamlConfiguration();
                 fileConfiguration.save(configFile);
                 return;
             }
-            Files.copy(in, configFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
+
+           if(!configFile.exists()) {
+               configFile.mkdirs();
+           }
+
+           fileConfiguration = new YamlConfiguration();
+
+           fileConfiguration.load(configFile);
+
+        } catch (IOException | InvalidConfigurationException e) {
             e.printStackTrace();
         }
     }
