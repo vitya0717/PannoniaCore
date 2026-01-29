e60846e7ed461ce3181daf47ce16ed7ded957810
vitya0717 <tviktor20000717@gmail.com>
2026-01-29 12:24:49 +0100

    Command manager class

--- src/main/java/org/vitya0717/pannoniaCore/command/CommandAPI.java
+++ src/main/java/org/vitya0717/pannoniaCore/command/CommandAPI.java
@@ -3,43 +3,42 @@ package org.vitya0717.pannoniaCore.command;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 
-import java.util.Collections;
 import java.util.List;
 
 public class CommandAPI extends Command {
 
-
-    public CommandAPI(String commandName, List<String> commandAliases, String permission, String description , String usageMessage) {
+    public CommandAPI(String commandName, List<String> commandAliases, String permission, String description, String usageMessage) {
         super(commandName);
-        this.setAliases(commandAliases); // Optional
-        this.setPermission(permission); // Optional
+        this.setAliases(commandAliases); // Optional: Add aliases
+        this.setPermission(permission); // Optional: Set a permission
         this.setDescription(description);
         this.setUsage(usageMessage);
     }
 
-    public CommandAPI(String commandName, String permission, String description , String usageMessage) {
+    public CommandAPI(String commandName, String permission, String description, String usageMessage) {
         super(commandName);
-        this.setAliases(List.of()); // Optional
-        this.setPermission(permission); // Optional
+        //this.setAliases(Collections.singletonList("cm")); // Optional: Add aliases
+        this.setPermission(permission); // Optional: Set a permission
         this.setDescription(description);
         this.setUsage(usageMessage);
     }
 
-    public CommandAPI(String commandName, String description , String usageMessage) {
+    public CommandAPI(String commandName, String description, String usageMessage) {
         super(commandName);
-        this.setAliases(List.of()); // Optional
-        //this.setPermission(null); // Optional
+        //this.setAliases(Collections.singletonList("cm")); // Optional: Add aliases
+        //this.setPermission("pannoniacore.chatmodule"); // Optional: Set a permission
         this.setDescription(description);
         this.setUsage(usageMessage);
     }
 
     public CommandAPI(String commandName, String description) {
         super(commandName);
-        this.setAliases(List.of()); // Optional
-        //this.setPermission(null); // Optional
+        //this.setAliases(Collections.singletonList("cm")); // Optional: Add aliases
+        //this.setPermission("pannoniacore.chatmodule"); // Optional: Set a permission
         this.setDescription(description);
-        //this.setUsage("");
+        //this.setUsage("/chatmodule");
     }
+
 
     @Override
     public boolean execute(CommandSender sender, String s, String[] args) {
@@ -47,8 +46,7 @@ public class CommandAPI extends Command {
     }
 
     @Override
-    public List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException {
-        return Collections.emptyList();
+    public List<String> tabComplete(CommandSender sender, String alias, String[] args) {
+        return List.of();
     }
-
 }
